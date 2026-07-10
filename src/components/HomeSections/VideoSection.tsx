39db849240d6f30099db692d1c6141ec3020e290
hclhcl0 <hclhcl0@gmail.com>
2026-07-11 00:34:43 +0700

    fix: add missing Link import in VideoSection causing build failure

--- src/components/HomeSections/VideoSection.tsx
+++ src/components/HomeSections/VideoSection.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import { getPayload } from 'payload';
 import configPromise from '@payload-config';
 import { VideoCardPopup } from './VideoCardPopup';
